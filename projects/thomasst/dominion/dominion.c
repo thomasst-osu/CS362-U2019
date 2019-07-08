202802ec3a5e6160c93aa176953e66d03815c825
Stephen Thomas <thomasst@oregonstate.edu>
2019-07-08 00:36:43 -0500

    Update dominion.c
    Deleted old mine card code in cardEffect function.

--- projects/thomasst/dominion/dominion.c
+++ projects/thomasst/dominion/dominion.c
@@ -769,41 +769,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
     case mine:
       return playMineCard(choice1, choice2, currentPlayer, handPos, state);
-    /*
-      j = state->hand[currentPlayer][choice1];  //store card we will trash
-
-      if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
-	{
-	  return -1;
-	}
-
-      if (choice2 > treasure_map || choice2 < curse)
-	{
-	  return -1;
-	}
-
-      if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
-	{
-	  return -1;
-	}
-
-      gainCard(choice2, state, 2, currentPlayer);
-
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-
-      //discard trashed card
-      for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == j)
-	    {
-	      discardCard(i, currentPlayer, state, 0);
-	      break;
-	    }
-	}
-
-      return 0;
-*/
+  
     case remodel:
       j = state->hand[currentPlayer][choice1];  //store card we will trash
 
